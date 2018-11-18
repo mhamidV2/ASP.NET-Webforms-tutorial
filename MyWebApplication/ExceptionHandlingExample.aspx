58120444a455e3c87adeeb80910dbaaa888d96ce
M'zé <mzehamid@outlook.fr>
2018-11-18 18:02:19 +0100

    Exception Handling: Try catch or Error page

---
+++ MyWebApplication/ExceptionHandlingExample.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ExceptionHandlingExample.aspx.cs" Inherits="MyWebApplication.ExceptionHandlingExample" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Exception Handling Example</h1>
+    <asp:Label ID="lblMessage" CssClass="alert-danger" runat="server" Visible="False"></asp:Label>
+    <div class="form-group">
+        <label>This should be a decimal: </label>
+        <asp:TextBox ID="txtDecimal" CssClass="form-control" runat="server"/>
+    </div>
+    <div class="form-group">
+        <asp:Button ID="btnSubmit" CssClass="btn btn-success" Text="Submit" OnClick="btnSubmit_OnClick" runat="server"/>
+    </div>
+</asp:Content>
---
+++ MyWebApplication/ExceptionHandlingExample.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyWebApplication
+{
+    public partial class ExceptionHandlingExample : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_OnClick(object sender, EventArgs e)
+        {
+            string message = String.Empty;
+            //try
+            //{
+                decimal expectedDecimal = decimal.Parse(txtDecimal.Text.Replace(".",","));
+                message = "Your decimal is: " + expectedDecimal;
+            //}
+            //catch(Exception ex)
+            //{
+            //    message = "Something went wrong: "+ex.Message;
+            //}
+            lblMessage.Text = message;
+            lblMessage.Visible = true;
+        }
+    }
+}
