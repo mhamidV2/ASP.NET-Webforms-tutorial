74a8ba421f22baf75e010b5b189611f2318fbb0f
mhamidV2 <36427962+mhamidV2@users.noreply.github.com>
2018-11-18 18:04:28 +0100

    Exception Handling: Try catch or Error page (#4)

---
+++ MyWebApplication/ErrorPage.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ErrorPage.aspx.cs" Inherits="MyWebApplication.ErrorPage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="jumbotron">
+        <h1>Ouch !</h1>
+        <p class="lead">An error just happened, sorry for the inconvenience :(</p>
+    </div>
+</asp:Content>
---
+++ MyWebApplication/ExceptionHandlingExample.aspx.designer.cs
@@ -0,0 +1,42 @@
+﻿//------------------------------------------------------------------------------
+// <généré automatiquement>
+//     Ce code a été généré par un outil.
+//
+//     Les modifications apportées à ce fichier peuvent provoquer un comportement incorrect et seront perdues si
+//     le code est régénéré.
+// </généré automatiquement>
+//------------------------------------------------------------------------------
+
+namespace MyWebApplication {
+    
+    
+    public partial class ExceptionHandlingExample {
+        
+        /// <summary>
+        /// Contrôle lblMessage.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMessage;
+        
+        /// <summary>
+        /// Contrôle txtDecimal.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDecimal;
+        
+        /// <summary>
+        /// Contrôle btnSubmit.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSubmit;
+    }
+}
